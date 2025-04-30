c99a111496e02608b330c772998efa3cb8d55502
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-04-30 00:19:59 -0400

    CRUD operations

--- springbootJDBC/src/main/java/com/springbootJDBC/springbootJDBC/DAO/UserDAO.java
+++ springbootJDBC/src/main/java/com/springbootJDBC/springbootJDBC/DAO/UserDAO.java
@@ -4,6 +4,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.springbootJDBC.springbootJDBC.entity.User;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 @Repository
 public class UserDAO{
 
@@ -24,4 +27,40 @@ public class UserDAO{
 
         return status;
     }
+
+    public Boolean updateUser(User user, String email){
+        boolean status=false;
+        try{
+            String updateQuery="UPDATE users set email=?,name=?,gender=?,city=? where email=?";
+            int count=jdbcTemplate.update(updateQuery, user.getEmail(),user.getName(),user.getGender(),user.getCity(),user.getEmail());
+            status= (count >0) ? true: false;
+        } catch (Exception e) {
+            status=false;
+        }
+        return status;
+    }
+
+    public Boolean deleteUser(String email){
+        boolean status= false;
+        try{
+            String deleteUserQuery="Delete from users where email=?";
+            int count= jdbcTemplate.update(deleteUserQuery,email);
+            status= (count >0) ? true: false;
+        } catch (Exception e) {
+            status = false;
+        }
+        return status;
+    }
+
+//    public Boolean getUsers(String email){
+//        String selectQuery=jdbcTemplate.query("Select * from users where email=?",email);
+//        return jdbcTemplate.query(selectQuery,);
+//    }
+//
+//    public static final class UserRowMapper implements RowMapper<User>{
+//
+//        public User mapRow(ResultSet rs,int rowNum) throws SQLException{
+//            User user = new User();
+//        }
+//    }
 }
